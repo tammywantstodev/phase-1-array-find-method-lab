17a68c5ce03374e0f4a32c89154e26d9d63268e1
Tamara Mwanzali Kaka <billnyetherussianspy679@gmail.com>
2024-11-26 21:45:19 +0300

    Saving changes

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+// code your solution here
+//function should return year of W
+//return undefined if nonne
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+]
+function superbowlWin(arr){
+    for(const win of arr){
+       if( win.result==="W"){
+           return win.year
+       }
+    }
+    return undefined
+}
